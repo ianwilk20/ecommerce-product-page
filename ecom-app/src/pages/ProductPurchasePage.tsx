30b6c417cbe7b40d57341b50e6e5c750cf2aa25a
ianwilk20 <47506745+ianwilk20@users.noreply.github.com>
2024-11-26 22:01:12 -0500

    Initial commit

---
+++ ecom-app/src/pages/ProductPurchasePage.tsx
@@ -0,0 +1,16 @@
+const ProductPurchasePage = () => {
+
+    return (
+        <img />
+        <span>{brand}</span>
+        <h2>{product_name}</h2>
+        <p>{product_description}</p>
+        <div>
+        <span>{cost}</span>
+        <span>{percent_off}</span>
+        <span>{sale_price}</span>
+        </div>
+    )
+}
+
+export default ProductPurchasePage
